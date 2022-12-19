892833dc796fd7a00d0609368dbc4a8f0c181398
Char_marJudge <19307110343@fudan.edu.cn>
2022-12-19 11:16:54 +0800

    comment well

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -11,6 +11,8 @@ from get_last_week import get_last_week, get_detail_info
 from user import User, get_user, create_user
 from databaseCURD import getDatabase, commitChangeToDatabase
 import json
+from comment import comment_model
+import time
 
 def create_app(test_config=None):
     # create and configure the app
@@ -113,7 +115,7 @@ def create_app(test_config=None):
 
 
 
-    @app.route('/bangumi')
+    @app.route('/bangumi/')
     def get_bangumi_info():
         agrs = request.args
         bangumi = None
@@ -127,6 +129,37 @@ def create_app(test_config=None):
     def favicon():
         print('favicon fun called!')
         return current_app.send_static_file('images/favicon.ico')
+
+    @app.route('/yukiyu/comment/<bangumi_id>')
+    def comment(bangumi_id):
+        row = comment_model("select * from comment where bangumi_id = %d"%(int(bangumi_id)))
+        if hasattr(current_user, 'username'):
+            userame = current_user.username
+        if row == 0 or None:
+            flag = 0
+        else:
+            flag = 1
+        return render_template('comment.html', data=row, flag = flag)
+
+    @app.route('/insert/<bangumi_id>', methods=['POST'])
+    def insert(bangumi_id):
+        # 1.接收表单数据
+        data = request.form.to_dict() 
+        data['date'] = time.strftime('%Y-%m-%d %H:%M:%S')
+        print(data)
+        # 2.把数据添加到数据库
+        if hasattr(current_user, 'username'):
+            userame = current_user.username
+        print(userame)
+        sql = f'insert into comment values(null,"{userame}","{data["info"]}","{data["date"]}",{int(bangumi_id)},0)'
+        res = comment_model(sql)
+        print(res)
+
+        # 3.
+        if res:
+            return '<script>alert("发布成功！");location.href="/yukiyu/comment/%d"</script>'%(int(bangumi_id))
+        else:
+            return '<script>alert("发布失败！");location.href="/yukiyu/comment/%d"</script>'%(int(bangumi_id))
 
 
     return app
